4643e1ebc45fbdd4ca3cb45a83e9d269b17cc1b0
varserith <varserith@localhost>
2018-05-13 17:38:29 -0300

    Reformat

--- src/Data/QualityInfo.java
+++ src/Data/QualityInfo.java
@@ -11,87 +11,87 @@ package Data;
  */
 public class QualityInfo extends Object
 {
-	private String _code;
-	private String _name;
-	private String _resolution;
-	private String _size;
-	
-	// CONSTRUCTORES //
-	
-	public QualityInfo()
-	{
-	this._code = new String();
-	this._name = new String();
-	this._resolution = new String();
-	this._size = new String();
-	}
-	
-	public QualityInfo(String _code, String _name)
-	{
-		this._code = _code;
-		this._name = _name;
-		this._resolution = new String();
-		this._size = new String();
-	}
-	
-	public QualityInfo(String _code, String _name, String _resolution, String _size)
-	{
-		this._code = _code;
-		this._name = _name;
-		this._resolution = _resolution;
-		this._size = _size;
-	}
-	
-	// METODOS EXTRA //
-	@Override public String toString()
-	{
-		return _size.equals("") ? _name : _name + " [" + _size +"]";
-	}
-	
-	public String printingString()
-	{
-		return _code + "\n" + _name + "\n" + _resolution + "\n" + _size;
-	}
-	
-	// GETTERS & SETTERS //
-	public String getCode()
-	{
-		return _code;
-	}
+    private String _code;
+    private String _name;
+    private String _resolution;
+    private String _size;
 
-	public void setCode(String _code)
-	{
-		this._code = _code;
-	}
+    // CONSTRUCTORES //
+    public QualityInfo()
+    {
+        this._code = new String();
+        this._name = new String();
+        this._resolution = new String();
+        this._size = new String();
+    }
 
-	public String getName()
-	{
-		return _name;
-	}
+    public QualityInfo(String _code, String _name)
+    {
+        this._code = _code;
+        this._name = _name;
+        this._resolution = new String();
+        this._size = new String();
+    }
 
-	public void setName(String _name)
-	{
-		this._name = _name;
-	}
+    public QualityInfo(String _code, String _name, String _resolution, String _size)
+    {
+        this._code = _code;
+        this._name = _name;
+        this._resolution = _resolution;
+        this._size = _size;
+    }
 
-	public String getResolution()
-	{
-		return _resolution;
-	}
+    // METODOS EXTRA //
+    @Override
+    public String toString()
+    {
+        return _size.equals("") ? _name : _name + " [" + _size + "]";
+    }
 
-	public void setResolution(String _resolution)
-	{
-		this._resolution = _resolution;
-	}
+    public String printingString()
+    {
+        return _code + "\n" + _name + "\n" + _resolution + "\n" + _size;
+    }
 
-	public String getSize()
-	{
-		return _size;
-	}
+    // GETTERS & SETTERS //
+    public String getCode()
+    {
+        return _code;
+    }
 
-	public void setSize(String _size)
-	{
-		this._size = _size;
-	}
-	
+    public void setCode(String _code)
+    {
+        this._code = _code;
+    }
+
+    public String getName()
+    {
+        return _name;
+    }
+
+    public void setName(String _name)
+    {
+        this._name = _name;
+    }
+
+    public String getResolution()
+    {
+        return _resolution;
+    }
+
+    public void setResolution(String _resolution)
+    {
+        this._resolution = _resolution;
+    }
+
+    public String getSize()
+    {
+        return _size;
+    }
+
+    public void setSize(String _size)
+    {
+        this._size = _size;
+    }
+
 }
--- src/GUI/YoutubeDl.java
+++ src/GUI/YoutubeDl.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package GUI;
+
 import Data.QualityInfo;
 
 import java.io.BufferedReader;
@@ -24,131 +25,128 @@ import javax.swing.JTextArea;
  */
 public class YoutubeDl
 {
-	public static void updateQualityList(JComboBox qualityList, String URL, JProgressBar progressBar)
-	{
-		progressBar.setIndeterminate(true);
-		ArrayList<String> _comArray = new ArrayList<>();
-		
-		if (System.getProperty("os.name").contains("Windows"))
-			_comArray.add("./tools/youtube-dl.exe");
-		else
-			_comArray.add("youtube-dl");
-		
-		_comArray.add("-F");
-		_comArray.add(URL);
-		try
-		{
-			Process yt_dl = new ProcessBuilder().command(_comArray).inheritIO().redirectErrorStream(true).redirectOutput(ProcessBuilder.Redirect.PIPE).start();
-			
-			BufferedReader _reader = new BufferedReader(new InputStreamReader(yt_dl.getInputStream()));
-			String line;
-			
-			ArrayList<QualityInfo> _lista_calidades = new ArrayList<>();
-			
-			while ((line = _reader.readLine()) != null)
-			{
-				List<String> _info = Arrays.asList(line.split(",? +,* *"));
-				if (_info.contains("mp4") && !_info.toString().contains("mp4a"))
-				{
-					_lista_calidades.add(new QualityInfo(_info.get(0), _info.get(3), _info.get(2), _info.get(9)));
-				}
-			}
-			for (QualityInfo q : _lista_calidades)
-			{
-				qualityList.addItem(q);
-			}
-		}
-		catch (IOException e)
-		{
-			JOptionPane.showMessageDialog(null, "Error en la ejecucion de youtube-dl. ERROR: " + e.getMessage(), "Error", JOptionPane.WARNING_MESSAGE);
-		}
-		progressBar.setIndeterminate(false);
-	}
-	
-	public static void updateYTDL(JTextArea outputArea, JProgressBar progressBar)
-	{
-		progressBar.setIndeterminate(true);
-		ArrayList<String> _comArray = new ArrayList<>();
-		if (System.getProperty("os.name").contains("Windows"))
-			_comArray.add("./tools/youtube-dl.exe");
-		else
-			_comArray.add("youtube-dl");
-		
-		_comArray.add("-U");
-		
-		try
-		{
-			Process yt_dl = new ProcessBuilder().command(_comArray).inheritIO().redirectErrorStream(true).redirectOutput(ProcessBuilder.Redirect.PIPE).start();
-			
-			BufferedReader _reader = new BufferedReader(new InputStreamReader(yt_dl.getInputStream()));
-			String line;
-			
-			while ((line = _reader.readLine()) != null)
-			{
-				outputArea.append(line + "\n");
-				//Autoscroll:
-				outputArea.setCaretPosition(outputArea.getDocument().getLength() - 1);
-			}
-			
-		}
-		catch (IOException e)
-		{
-			JOptionPane.showMessageDialog(null, "Error en la ejecucion de youtube-dl. ERROR: " + e.getMessage(), "Error", JOptionPane.WARNING_MESSAGE);
-		}
-		progressBar.setIndeterminate(false);
-	}
-	
-	public static void getVideo(String URL, ArrayList Options, String Output, JProgressBar progressBar, JLabel speedLabel, JTextArea outputArea, JComboBox comboBox)
-	{
-		ArrayList<String> _comArray = new ArrayList<>();
-		if (System.getProperty("os.name").contains("Windows"))
-		{
-			_comArray.add("./tools/youtube-dl.exe");
-		}
-		else
-			_comArray.add("youtube-dl");
-		
-		_comArray.addAll(Options);
-		_comArray.add(URL);
-		_comArray.add("-o");
-		_comArray.add(Output);
+    public static void updateQualityList(JComboBox qualityList, String URL, JProgressBar progressBar)
+    {
+        progressBar.setIndeterminate(true);
+        ArrayList<String> _comArray = new ArrayList<>();
 
-		try
-		{
-			Process yt_dl = new ProcessBuilder().command(_comArray).inheritIO().redirectErrorStream(true).redirectOutput(ProcessBuilder.Redirect.PIPE).start();
-			
-			BufferedReader _reader = new BufferedReader(new InputStreamReader(yt_dl.getInputStream()));
-			String line;
-			
-			progressBar.setMinimum(0);
-			progressBar.setMaximum(100);
-			Float value;
-			
-			while ((line = _reader.readLine()) != null)
-			{
-				outputArea.append(line + "\n");
-				//Autoscroll:
-				outputArea.setCaretPosition(outputArea.getDocument().getLength() - 1);
-				
-				if (line.contains("[download]") && !line.contains("Destination"))
-				{
-					for (String x : line.split(" "))
-					{
-						if(x.contains("%"))
-						{
-							value = Float.parseFloat(x.trim().replaceAll("%", ""));
-							progressBar.setValue(value.intValue());
-						}
-						if(x.contains("/s"))
-							speedLabel.setText(x.trim());
-					}
-				}
-			}
-			speedLabel.setText("Listo!");
-		}
-		catch (IOException e)
-		{
-			JOptionPane.showMessageDialog(null, "Error en la ejecucion de youtube-dl. ERROR: " + e.getMessage(), "Error", JOptionPane.WARNING_MESSAGE);
-		}
-	}
+        if (System.getProperty("os.name").contains("Windows"))
+            _comArray.add("./tools/youtube-dl.exe");
+        else
+            _comArray.add("youtube-dl");
+
+        _comArray.add("-F");
+        _comArray.add(URL);
+        try
+        {
+            Process yt_dl = new ProcessBuilder().command(_comArray).inheritIO().redirectErrorStream(true).redirectOutput(ProcessBuilder.Redirect.PIPE).start();
+
+            BufferedReader _reader = new BufferedReader(new InputStreamReader(yt_dl.getInputStream()));
+            String line;
+
+            ArrayList<QualityInfo> _lista_calidades = new ArrayList<>();
+
+            while ((line = _reader.readLine()) != null)
+            {
+                List<String> _info = Arrays.asList(line.split(",? +,* *"));
+                if (_info.contains("mp4") && !_info.toString().contains("mp4a"))
+                {
+                    _lista_calidades.add(new QualityInfo(_info.get(0), _info.get(3), _info.get(2), _info.get(9)));
+                }
+            }
+            for (QualityInfo q : _lista_calidades)
+            {
+                qualityList.addItem(q);
+            }
+        } catch (IOException e)
+        {
+            JOptionPane.showMessageDialog(null, "Error en la ejecucion de youtube-dl. ERROR: " + e.getMessage(), "Error", JOptionPane.WARNING_MESSAGE);
+        }
+        progressBar.setIndeterminate(false);
+    }
+
+    public static void updateYTDL(JTextArea outputArea, JProgressBar progressBar)
+    {
+        progressBar.setIndeterminate(true);
+        ArrayList<String> _comArray = new ArrayList<>();
+        if (System.getProperty("os.name").contains("Windows"))
+            _comArray.add("./tools/youtube-dl.exe");
+        else
+            _comArray.add("youtube-dl");
+
+        _comArray.add("-U");
+
+        try
+        {
+            Process yt_dl = new ProcessBuilder().command(_comArray).inheritIO().redirectErrorStream(true).redirectOutput(ProcessBuilder.Redirect.PIPE).start();
+
+            BufferedReader _reader = new BufferedReader(new InputStreamReader(yt_dl.getInputStream()));
+            String line;
+
+            while ((line = _reader.readLine()) != null)
+            {
+                outputArea.append(line + "\n");
+                //Autoscroll:
+                outputArea.setCaretPosition(outputArea.getDocument().getLength() - 1);
+            }
+
+        } catch (IOException e)
+        {
+            JOptionPane.showMessageDialog(null, "Error en la ejecucion de youtube-dl. ERROR: " + e.getMessage(), "Error", JOptionPane.WARNING_MESSAGE);
+        }
+        progressBar.setIndeterminate(false);
+    }
+
+    public static void getVideo(String URL, ArrayList Options, String Output, JProgressBar progressBar, JLabel speedLabel, JTextArea outputArea, JComboBox comboBox)
+    {
+        ArrayList<String> _comArray = new ArrayList<>();
+        if (System.getProperty("os.name").contains("Windows"))
+        {
+            _comArray.add("./tools/youtube-dl.exe");
+        }
+        else
+            _comArray.add("youtube-dl");
+
+        _comArray.addAll(Options);
+        _comArray.add(URL);
+        _comArray.add("-o");
+        _comArray.add(Output);
+
+        try
+        {
+            Process yt_dl = new ProcessBuilder().command(_comArray).inheritIO().redirectErrorStream(true).redirectOutput(ProcessBuilder.Redirect.PIPE).start();
+
+            BufferedReader _reader = new BufferedReader(new InputStreamReader(yt_dl.getInputStream()));
+            String line;
+
+            progressBar.setMinimum(0);
+            progressBar.setMaximum(100);
+            Float value;
+
+            while ((line = _reader.readLine()) != null)
+            {
+                outputArea.append(line + "\n");
+                //Autoscroll:
+                outputArea.setCaretPosition(outputArea.getDocument().getLength() - 1);
+
+                if (line.contains("[download]") && !line.contains("Destination"))
+                {
+                    for (String x : line.split(" "))
+                    {
+                        if (x.contains("%"))
+                        {
+                            value = Float.parseFloat(x.trim().replaceAll("%", ""));
+                            progressBar.setValue(value.intValue());
+                        }
+                        if (x.contains("/s"))
+                            speedLabel.setText(x.trim());
+                    }
+                }
+            }
+            speedLabel.setText("Listo!");
+        } catch (IOException e)
+        {
+            JOptionPane.showMessageDialog(null, "Error en la ejecucion de youtube-dl. ERROR: " + e.getMessage(), "Error", JOptionPane.WARNING_MESSAGE);
+        }
+    }
 }
